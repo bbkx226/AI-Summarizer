36afc85a981c3fc325972cad3d9ab2e78e7cf73b
Brandon <bbkx226@gmail.com>
2023-06-29 14:03:43 +0800

    Design UI

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { copy, linkIcon, loader, tick } from '../assets'
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  })
+
+  const handleSubmit = async (e) => {
+    alert('Submitted')
+  }
+
   return (
-    <div>Demo</div>
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img 
+            src={linkIcon}
+            alt="link-icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+
+          <input 
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            <p>↵</p>
+          </button>
+        </form>
+
+      </div>
+    </section>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
 
-export default Hero
+        <button
+          type='button'
+          onClick={() =>
+            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+          }
+          className='black_btn'
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient '>BrandonGPT</span>
+      </h1>
+      <h2 className='desc'>
+        Discover BrandonGPT - Articles Summarizer, a free and open-source tool 
+        designed to simplify your reading experience. This innovative article 
+        summarizer efficiently condenses lengthy articles into easily understandable 
+        and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
